9b6572d357042354423f1296439371081c9070bf
creturn <master@creturn.com>
2014-01-07 17:12:13 +0800

    添加从摄像头获取图片大图方法

--- PictureEffect/src/com/creturn/pictureeffect/MainActivity.java
+++ PictureEffect/src/com/creturn/pictureeffect/MainActivity.java
@@ -1,12 +1,16 @@
 package com.creturn.pictureeffect;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -64,16 +68,17 @@ public class MainActivity extends Activity {
 				 Intent intent=new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 				 //这里获取大图原理，先把相机拍的照片存到固定目录，然后读取时候从保存路径读取即可，不用直接通过内存传输照片
 				 //通过intent传递尺寸会被压缩
-//				 intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile(F.SD_CARD_TEMP_PHOTO_PATH)));
+				 intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"img_tmp.jpg")));
 				 startActivityForResult(intent, CAMERA_RESULT);
 			}
 		});
-		//信息按钮
+		//复古处理
 		btn_info.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				Log.i(LOG_NAME,"info click" + Environment.getExternalStorageDirectory().getPath());
-				
+				Log.i(LOG_NAME,"old remember" + Environment.getExternalStorageDirectory().getPath());
+				Bitmap newpic = oldRemeber(((BitmapDrawable)imgView.getDrawable()).getBitmap());
+				imgView.setImageBitmap(newpic);
 			}
 		});
 	}
@@ -117,19 +122,76 @@ public class MainActivity extends Activity {
 		}
 		
 		if(requestCode == CAMERA_RESULT && resultCode == RESULT_OK && null != data){
+			/**
+			 * 和上面调用一样默认调用相机不加保存位置
+			 * 返回的是经过压缩的图片
+			 * 
 			Bundle extras = data.getExtras();
 			Bitmap bmpBitmap=(Bitmap)extras.get("data");
 			imgView.setImageBitmap(bmpBitmap);
+			*/
+			Bitmap bitmap =  BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "img_tmp.jpg");
 			
-			
-			//可以保存到相册中
-			String url = MediaStore.Images.Media.insertImage(getContentResolver(), bmpBitmap, "", "");
-			Log.i(LOG_NAME, "Image Save Path:" + url);
+			//保存到相册中
+			MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "", "");
+			Bitmap nBitmap = ScalePic(bitmap, bitmap.getWidth()/2, bitmap.getHeight()/2);
+			bitmap.recycle();
+			imgView.setImageBitmap(nBitmap);
+//			Log.i(LOG_NAME, "Image Save Path:" + url);
 			//检测sdk可用不，可用就写入图片到sdk
 			
 		}
 	}
 	
+	/**
+	 * 怀旧效果(相对之前做了优化快一倍)
+	 * @param bmp
+	 * @return
+	 */
+	public Bitmap oldRemeber(Bitmap bmp)
+	{
+		// 速度测试
+		long start = System.currentTimeMillis();
+		int width = bmp.getWidth();
+		int height = bmp.getHeight();
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+		int pixColor = 0;
+		int pixR = 0;
+		int pixG = 0;
+		int pixB = 0;
+		int newR = 0;
+		int newG = 0;
+		int newB = 0;
+		int[] pixels = new int[width * height];
+		bmp.getPixels(pixels, 0, width, 0, 0, width, height);
+		for (int i = 0; i < height; i++)
+		{
+			for (int k = 0; k < width; k++)
+			{
+				pixColor = pixels[width * i + k];
+				pixR = Color.red(pixColor);
+				pixG = Color.green(pixColor);
+				pixB = Color.blue(pixColor);
+				newR = (int) (0.393 * pixR + 0.769 * pixG + 0.189 * pixB);
+				newG = (int) (0.349 * pixR + 0.686 * pixG + 0.168 * pixB);
+				newB = (int) (0.272 * pixR + 0.534 * pixG + 0.131 * pixB);
+				int newColor = Color.argb(255, newR > 255 ? 255 : newR, newG > 255 ? 255 : newG, newB > 255 ? 255 : newB);
+				pixels[width * i + k] = newColor;
+			}
+		}
+		
+		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+		long end = System.currentTimeMillis();
+		Log.d("may", "used time="+(end - start));
+		return bitmap;
+	}
+	/**
+	 * 缩小图片	
+	 * @param bmp
+	 * @param ScaleWidth
+	 * @param ScaleHeight
+	 * @return
+	 */
 	public Bitmap ScalePic(Bitmap bmp, float ScaleWidth, float ScaleHeight){
 		Matrix matrix = new Matrix();
 		matrix.postScale(ScaleWidth/bmp.getWidth(), ScaleHeight/bmp.getHeight());
